add15db939e5fcef12335611241e310249495be6
Kristania <kristania@example.com>
2024-10-26 15:52:59 +0700

    AboutUsActivity

--- app/src/main/java/com/example/stylo/AboutUsActivity.kt
+++ app/src/main/java/com/example/stylo/AboutUsActivity.kt
@@ -23,7 +23,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-//import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
